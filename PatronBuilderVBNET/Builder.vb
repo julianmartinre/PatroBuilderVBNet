64d047359fd133061b4c6d163ff6f3deece819a4
julianmartinre <julian.martin.93@gmail.com>
2017-09-25 12:17:58 -0300

    initial commit

---
+++ PatronBuilderVBNET/Builder.vb
@@ -0,0 +1,101 @@
+﻿Imports PatronBuilderVBNET
+
+Public Class Builder
+
+    Class Concecionaria
+
+        Dim motoBuilder As MotoBuilder
+
+        Public Sub CrearMoto(unBuilder As MotoBuilder)
+
+            motoBuilder = unBuilder
+            unBuilder.ConstruirMotor()
+            unBuilder.ConstruirRuedas()
+
+        End Sub
+
+    End Class
+
+    MustInherit Class MotoBuilder
+        Public MustOverride Sub ConstruirMotor()
+        Public MustOverride Sub ConstruirRuedas()
+        Public MustOverride Function GetMoto() As Moto
+
+    End Class
+
+    Class ScooterMotoBuilder
+        Inherits MotoBuilder
+
+        Private unaMoto As New Moto
+
+        Public Overrides Sub ConstruirMotor()
+            unaMoto.add("MotorScooter")
+        End Sub
+
+        Public Overrides Sub ConstruirRuedas()
+            unaMoto.add("RuedasScooter")
+        End Sub
+
+        Public Overrides Function GetMoto() As Moto
+            Return unaMoto
+        End Function
+    End Class
+
+    Class EnduroMotoBuilder
+        Inherits MotoBuilder
+
+        Private unaMoto As New Moto
+
+        Public Overrides Sub ConstruirMotor()
+            unaMoto.add("EnduroMotor")
+        End Sub
+
+        Public Overrides Sub ConstruirRuedas()
+            unaMoto.add("EnduroRuedas")
+        End Sub
+
+        Public Overrides Function GetMoto() As Moto
+            Return unaMoto
+        End Function
+    End Class
+
+    Class PisteroMotoBuilder
+        Inherits MotoBuilder
+
+        Private unaMoto As New Moto
+
+        Public Overrides Sub ConstruirMotor()
+            unaMoto.add("PisteroMotor")
+        End Sub
+
+        Public Overrides Sub ConstruirRuedas()
+            unaMoto.add("PisteroRuedas")
+        End Sub
+
+        Public Overrides Function GetMoto() As Moto
+            Return unaMoto
+        End Function
+    End Class
+
+    Class Moto
+
+        Dim partes As New ArrayList
+
+        Public Sub add(parte As String)
+            partes.Add(parte)
+        End Sub
+
+        Public Sub crearMoto()
+
+            Console.WriteLine("\Moto")
+
+            For Each item In partes
+                Console.WriteLine("" + item)
+                Console.WriteLine("")
+            Next
+
+        End Sub
+
+    End Class
+
+End Class
---
+++ PatronBuilderVBNET/Module1.vb
@@ -0,0 +1,34 @@
+﻿Imports PatronBuilderVBNET.Builder
+
+Module Module1
+
+    Sub Main()
+
+        Dim unaConcecionaria As New Concecionaria
+
+        Dim ScooterMotoBuilder As New ScooterMotoBuilder
+        Dim EnduroMotoBuilder As New EnduroMotoBuilder
+        Dim PisteroMotoBuilder As New PisteroMotoBuilder
+
+        Dim unaMoto As New Moto
+
+        unaConcecionaria.CrearMoto(ScooterMotoBuilder)
+
+        unaMoto = ScooterMotoBuilder.GetMoto()
+        unaMoto.crearMoto()
+
+        unaConcecionaria.CrearMoto(EnduroMotoBuilder)
+
+        unaMoto = EnduroMotoBuilder.GetMoto()
+        unaMoto.crearMoto()
+
+        unaConcecionaria.CrearMoto(PisteroMotoBuilder)
+
+        unaMoto = PisteroMotoBuilder.GetMoto()
+        unaMoto.crearMoto()
+
+        Console.Read()
+
+    End Sub
+
+End Module
